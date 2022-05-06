350ea0dc767d5d1c82d1bc2be68b39a44599a48e
Toru Yoshida <44675166+hidaytours@users.noreply.github.com>
2022-05-06 22:12:40 +0900

    fix lstclear, lstmap

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -16,13 +16,10 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*tmp;
 
-	if (del == NULL || lst == NULL || *lst == NULL)
+	if (lst == NULL || *lst == NULL || del == NULL)
 		return ;
 	tmp = *lst;
-	if (tmp->next != NULL)
-		*lst = tmp->next;
-	else
-		lst = NULL;
+	*lst = tmp->next;
 	ft_lstdelone(tmp, del);
 	ft_lstclear(lst, del);
 }
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -14,20 +14,18 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*l;
 	t_list	*mapped_head;
 	t_list	*mapped;
 
-	if (lst == NULL || f == NULL || del == NULL)
+	if (lst == NULL || f == NULL)
 		return (NULL);
-	l = lst;
-	mapped_head = ft_lstnew(f(l->content));
+	mapped_head = ft_lstnew(f(lst->content));
 	if (mapped_head == NULL)
 		return (NULL);
-	while (l->next != NULL)
+	while (lst->next != NULL)
 	{
-		l = l->next;
-		mapped = ft_lstnew(f(l->content));
+		lst = lst->next;
+		mapped = ft_lstnew(f(lst->content));
 		if (mapped == NULL)
 		{
 			ft_lstclear(&mapped_head, del);
