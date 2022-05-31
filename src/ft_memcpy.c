9623c1133fc7a55f3256ea494506c056d4da8ca1
Toru Yoshida <44675166+hidaytours@users.noreply.github.com>
2022-06-01 02:51:00 +0900

    fix

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	const unsigned char	*src_uc;
