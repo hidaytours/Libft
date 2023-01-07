b87dd27ea6fbddb41252abd53dbbbc78d527aec9
Toru Yoshida <44675166+hidaytours@users.noreply.github.com>
2023-01-07 15:24:54 +0900

    bugfix: Fix extra memory allocation @ft_strndup.c

--- src/string/ft_strndup.c
+++ src/string/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: toryoshi </var/mail/toryoshi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 16:45:26 by toryoshi          #+#    #+#             */
-/*   Updated: 2022/04/22 16:45:44 by toryoshi         ###   ########.fr       */
+/*   Updated: 2023/01/07 15:20:52 by toryoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 char	*ft_strndup(const char *s1, size_t n)
 {
+	size_t	len;
 	char	*copy;
 
 	if (n == SIZE_MAX)
 		return (NULL);
+	len = ft_strlen(s1);
+	if (n > len)
+		n = len;
 	copy = (char *)malloc(sizeof(char) * (n + 1));
 	if (copy == NULL)
 		return (NULL);
